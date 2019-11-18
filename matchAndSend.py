47896d617988d71faa3957bc3459668b16596ffe
Bohao(Aaron) Wang <aaronwang0407@gmail.com>
2019-11-17 21:46:41 -0500

    Add br_info1 into bell ringer email message

--- matchAndSend.py
+++ matchAndSend.py
@@ -1,5 +1,6 @@
 import BellRingMatch as m
 import gmailAuto as g
+import codecs
 
 #---------------------------------------------extract bell ringers and listeners------------------------------------------------------------
 # bellRingers = m.read_xls('Data/newForm.xls')
@@ -16,6 +17,12 @@ for i in listeners:
 matching_result_list = m.match_all(listeners, bellRingers)
 
 #----------------------------------------------------------send gmail-------------------------------------------------------------------------
+#Read texts
+#解铃人须知
+file1 = codecs.open("Data/br_info1.txt","r","utf-8")
+br_info1 = file1.read()
+file1.close()
+
 for matching_result in matching_result_list:
     #matching_resilt format :  [bell_ringer, matched_listener, time]
     bell_ringer = matching_result[0]
@@ -30,8 +37,8 @@ for matching_result in matching_result_list:
     if listener == -1:
         br_content = "Sorry, we cannot find a listener for you"
     else :
-        br_content = ("We have found you listener!\n   Your listener: " + listener.name + 
-            "\n   Wechat ID: " + listener.WID + "\n   At time: " + time)
+        br_content = "We have found you listener!\n   Wechat ID: " + listener.WID + "\n   At time: " + time
+        br_content += "\n\n" + br_reminder
     
     print("-->Sending email to Bell Ringer: " + bell_ringer.name + " at " + br_email + " ... ")
     g.sendGmail(br_content, br_email, title)
@@ -41,7 +48,8 @@ for matching_result in matching_result_list:
     if listener == -1:
          print("Does not need to send email to Listener")
     else :
-        l_content = "You have a matched bell ringer: " + bell_ringer.name + "\n At time: " + time
+        l_content = ("You have a matched bell ringer: " + bell_ringer.name + "\n At time: " + time +
+            "\n Email Address: " + bell_ringer.email)
         print("-->Sending email to Listener: " + listener.name + " at " + l_email + " ... ")
         g.sendGmail(l_content, l_email, title)
 
