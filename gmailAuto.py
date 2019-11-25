353180de5380b0cbaf1b7dfc1717bda23712cbb9
wbh123456 <35285095+wbh123456@users.noreply.github.com>
2019-11-24 22:11:37 -0500

    Fix email format

--- gmailAuto.py
+++ gmailAuto.py
@@ -1,5 +1,6 @@
 import smtplib
 from email.mime.text import MIMEText
+import BellRingMatch as m
 
 mail_host = 'smtp.gmail.com'
 mail_user = 'bellringtest@gmail.com'
@@ -28,3 +29,28 @@ def sendGmail(content,receiver,title):
         print('success')
     except smtplib.SMTPException as e:
         print('error',e)
+
+def generate_email_content(bell_ringer, listener, time):
+    #------bell ringer email content------
+    if listener != -1:
+        br_content = open("Data/email_contents/br_email_format.txt", encoding="gbk").read()
+        br_content =  br_content.replace("@X",bell_ringer.name,1)
+        br_content =  br_content.replace("@X",listener.name,1)
+        br_content =  br_content.replace("@X",time,1)
+    else:
+        br_content = "Sorry, we cannot find a listener for you"
+    # print(br_content)
+
+    #------listener email content------
+    if listener != -1:
+        l_content = open("Data/email_contents/l_email_format.txt", encoding="gbk").read()
+        l_content =  l_content.replace("@X",listener.name,1)
+        l_content =  l_content.replace("@X",bell_ringer.name,1)
+        l_content =  l_content.replace("@X",time,1)
+        l_content =  l_content.replace("@X",bell_ringer.name,1)
+        l_content =  l_content.replace("@X",bell_ringer.email,1)
+        l_content =  l_content.replace("@X",bell_ringer.topic,1)
+    else:
+        l_content = -1
+    # print(l_content)
+    return br_content,l_content
--- matchAndSend.py
+++ matchAndSend.py
@@ -1,6 +1,5 @@
 import BellRingMatch as m
 import gmailAuto as g
-import codecs
 
 #---------------------------------------------extract bell ringers and listeners------------------------------------------------------------
 # bellRingers = m.read_xls('Data/newForm.xls')
@@ -17,12 +16,6 @@ for i in listeners:
 matching_result_list = m.match_all(listeners, bellRingers)
 
 #----------------------------------------------------------send gmail-------------------------------------------------------------------------
-#Read texts
-#解铃人须知
-file1 = codecs.open("Data/br_info1.txt","r","utf-8")
-br_info1 = file1.read()
-file1.close()
-
 for matching_result in matching_result_list:
     #matching_resilt format :  [bell_ringer, matched_listener, time]
     bell_ringer = matching_result[0]
@@ -32,24 +25,17 @@ for matching_result in matching_result_list:
 
     br_email = bell_ringer.email
     l_email = listener.email
+    #generate email contents
+    br_content, l_content = g.generate_email_content(bell_ringer, listener,time)
 
-    #send email for bell ringer
-    if listener == -1:
-        br_content = "Sorry, we cannot find a listener for you"
-    else :
-        br_content = "We have found you listener!\n   Wechat ID: " + listener.WID + "\n   At time: " + time
-        br_content += "\n\n" + br_reminder
-    
+    #Send Emails
     print("-->Sending email to Bell Ringer: " + bell_ringer.name + " at " + br_email + " ... ")
     g.sendGmail(br_content, br_email, title)
 
-
-    #send email for bell ringer
-    if listener == -1:
-         print("Does not need to send email to Listener")
-    else :
-        l_content = ("You have a matched bell ringer: " + bell_ringer.name + "\n At time: " + time +
-            "\n Email Address: " + bell_ringer.email)
-        print("-->Sending email to Listener: " + listener.name + " at " + l_email + " ... ")
+    print("-->Sending email to Listener: " + listener.name + " at " + l_email + " ... ")
+    if l_content != -1:
         g.sendGmail(l_content, l_email, title)
+    else:
+        print("Does not need to send email to Listener")
+
 
