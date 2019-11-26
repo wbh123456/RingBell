6f97386b10870c31da3107bac3e1ff1fcc793325
Bohao(Aaron) Wang <aaronwang0407@gmail.com>
2019-11-25 22:35:31 -0500

    Fix email content
    Fix titles and add contents for listener not found condition

--- gmailAuto.py
+++ gmailAuto.py
@@ -8,7 +8,7 @@ mail_pass = 'EC123456789'
 sender = 'empowerchange.peerlistener@gmail.com'
 
 def sendGmail(content,receiver,title):
-    message = MIMEText(content,'plain','utf-8')
+    message = MIMEText(content,'plain','gbk')
     message['Subject'] = title 
 
     message['From'] = sender 
@@ -31,15 +31,19 @@ def sendGmail(content,receiver,title):
         print('error',e)
 
 def generate_email_content(bell_ringer, listener, time):
+    succeed_title = '解聆人线上倾听 (匹配成功 :)'
+    fail_title = '解聆人线上倾听 (匹配失败 :('
     #------bell ringer email content------
     if listener != -1:
         br_content = open("Data/email_contents/br_email_format.txt", encoding="gbk").read()
         br_content =  br_content.replace("@X",bell_ringer.name,1)
         br_content =  br_content.replace("@X",listener.name,1)
         br_content =  br_content.replace("@X",time,1)
+        title = succeed_title
     else:
-        br_content = "Sorry, we cannot find a listener for you"
-    # print(br_content)
+        br_content = open("Data/email_contents/br_email_format_fail.txt", encoding="gbk").read()
+        br_content =  br_content.replace("@X",bell_ringer.name,1)
+        title = fail_title
 
     #------listener email content------
     if listener != -1:
@@ -50,7 +54,8 @@ def generate_email_content(bell_ringer, listener, time):
         l_content =  l_content.replace("@X",bell_ringer.name,1)
         l_content =  l_content.replace("@X",bell_ringer.email,1)
         l_content =  l_content.replace("@X",bell_ringer.topic,1)
+        title = succeed_title
     else:
         l_content = -1
-    # print(l_content)
-    return br_content,l_content
+        title = -1
+    return br_content,l_content,title
--- matchAndSend.py
+++ matchAndSend.py
@@ -22,20 +22,17 @@ for matching_result in matching_result_list:
     bell_ringer = matching_result[0]
     listener = matching_result[1]
     time = matching_result[2]
-    title = 'Your matching result from Bell Ringer'
 
-    br_email = bell_ringer.email
-    l_email = listener.email
     #generate email contents
-    br_content, l_content = g.generate_email_content(bell_ringer, listener,time)
+    br_content, l_content, title = g.generate_email_content(bell_ringer, listener,time)
 
     #Send Emails
-    print("-->Sending email to Bell Ringer: " + bell_ringer.name + " at " + br_email + " ... ")
-    g.sendGmail(br_content, br_email, title)
+    print("-->Sending email to Bell Ringer: " + bell_ringer.name + " at " + bell_ringer.email + " ... ")
+    g.sendGmail(br_content, bell_ringer.email, title)
 
-    print("-->Sending email to Listener: " + listener.name + " at " + l_email + " ... ")
+    print("-->Sending email to Listener: " + listener.name + " at " + listener.email + " ... ")
     if l_content != -1:
-        g.sendGmail(l_content, l_email, title)
+        g.sendGmail(l_content, listener.email, title)
     else:
         print("Does not need to send email to Listener")
 
