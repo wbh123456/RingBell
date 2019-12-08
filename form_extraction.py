88a04a9a6312acce3bd73eb80f51f3be777d1020
wbh123456 <35285095+wbh123456@users.noreply.github.com>
2019-12-07 22:10:08 -0500

    Change downloading path and headless mode

--- form_extraction.py
+++ form_extraction.py
@@ -2,47 +2,96 @@ import time
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from pyvirtualdisplay import Display
+from selenium.webdriver.chrome.options import Options
 
-#pyanywhere test code
+# from pyvirtualdisplay import Display
+script_dir = os.path.dirname(os.path.realpath(__file__)) #<-- absolute dir the script is in
+abs_path = script_dir + '\\Data\\'
+
+# test code
 def test():
-    with Display():
-        # we can now start Firefox and it will run inside the virtual display
-        browser = webdriver.Firefox()
+    options = Options()
+    options.headless = True
+    
+    print("starting")
+    # we can now start Firefox and it will run inside the virtual display
+    browser = webdriver.Chrome(options=options)
 
-        # put the rest of our selenium code in a try/finally
-        # to make sure we always clean up at the end
-        try:
-            browser.get('https://www.google.com')
-            print(browser.title) #this should print title
+    # put the rest of our selenium code in a try/finally
+    # to make sure we always clean up at the end
+    try:
+        print("getting web")
+        browser.get('https://www.google.com')
+        print(browser.title) #this should print title
 
-        finally:
-            browser.quit()
+    finally:
+        browser.quit()
+        print("done")
 
-
-#form_extraction
-c = 0
-while (0):
-
+# form_extraction headless
+def extract_headless():
     start = time.time()
+    options = Options()
+    options.headless = True
 
-	driver = webdriver.Chrome(executable_path="./chromedriver.exe")
-    driver.get("https://www.askform.cn/login")
+    # options = webdriver.ChromeOptions()
+    prefs = {'download.default_directory' : abs_path}
+    options.add_experimental_option('prefs', prefs)
+    
+    print("starting browser...")
+    # we can now start Firefox and it will run inside the virtual display
+    browser = webdriver.Chrome(options=options)
 
-    driver.find_element_by_id("un").send_keys("contact.listener@gmail.com")
-    driver.find_element_by_id("pwd").send_keys("iamyourlistener")
-    driver.find_element_by_xpath(".//a[@onclick='askformLogin()']").click()
+    print("going to www.askform.cn/login ...")
+    browser.get("https://www.askform.cn/login")
 
-    driver.find_element_by_id("menu4603080002").click()
+    print("loggin in...")
+    browser.find_element_by_id("un").send_keys("contact.listener@gmail.com")
+    browser.find_element_by_id("pwd").send_keys("iamyourlistener")
+    browser.find_element_by_xpath(".//a[@onclick='askformLogin()']").click()
 
-    driver.find_element_by_xpath(".//a[@href='/Survey/DataList.aspx?AppConfigID=4603080002&FormApplicationID=10801670001&FormCategoryID=10833290001&FormID=16128410001']").click()
+    browser.find_element_by_id("menu4603080002").click()
 
-    driver.find_element_by_id("btnExport").click()
+    browser.find_element_by_xpath(".//a[@href='/Survey/DataList.aspx?AppConfigID=4603080002&FormApplicationID=10801670001&FormCategoryID=10833290001&FormID=16128410001']").click()
+    print("downloading...")
+    browser.find_element_by_id("btnExport").click()
 
     time.sleep(5)
-    driver.close()
+    browser.close()
 
     end = time.time()
-    c += 1
     print("time: " + str(end - start))
-    print("count: " + str(c))
+
+def extract():
+    #form_extraction
+    start = time.time()
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {'download.default_directory' : abs_path}
+    chrome_options.add_experimental_option('prefs', prefs)
+
+    print("starting browser...")
+    # we can now start Firefox and it will run inside the virtual display
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+
+    print("going to www.askform.cn/login ...")
+    browser.get("https://www.askform.cn/login")
+
+    print("loggin in...")
+    browser.find_element_by_id("un").send_keys("contact.listener@gmail.com")
+    browser.find_element_by_id("pwd").send_keys("iamyourlistener")
+    browser.find_element_by_xpath(".//a[@onclick='askformLogin()']").click()
+
+    browser.find_element_by_id("menu4603080002").click()
+
+    browser.find_element_by_xpath(".//a[@href='/Survey/DataList.aspx?AppConfigID=4603080002&FormApplicationID=10801670001&FormCategoryID=10833290001&FormID=16128410001']").click()
+    print("downloading...")
+    browser.find_element_by_id("btnExport").click()
+
+    time.sleep(5)
+    browser.close()
+
+    end = time.time()
+    print("time: " + str(end - start))
+
+extract()
+# extract_headless()
