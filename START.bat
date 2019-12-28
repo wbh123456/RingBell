1b65823acbe5c83a1b95f4026add8731ceb912f3
wbh123456 <35285095+wbh123456@users.noreply.github.com>
2019-12-28 15:06:55 -0800

    Revert logging start.log file
    logging start.log file would cause encoding problems in aws ec2 virtual
    machine, thus undo the logging process. After this commit, it would be
    the first working RingBell commit.

--- START.bat
+++ START.bat
@@ -1,9 +1,4 @@
 ﻿@echo off 
-set LOGFILE=START_log.log
-call :LOG > %LOGFILE%
-exit /B
-
-:LOG
 
 chcp 65001
 form_extraction.exe
@@ -11,3 +6,4 @@ del "Data\oldForm.xls"
 rename "Data\newForm.xls" oldForm.xls
 rename "Data\数据列表.xls" newForm.xls
 matchAndSend.exe
+pause
