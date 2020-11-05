05dfbac670bc7e665cc0d46e0107e62ff2212ab3
wbh123456 <35285095+wbh123456@users.noreply.github.com>
2020-11-05 14:57:55 -0500

    Set selenium timeout to 10min
    Signed-off-by: wbh123456 <35285095+wbh123456@users.noreply.github.com>

--- form_extraction.py
+++ form_extraction.py
@@ -69,7 +69,7 @@ def extract(is_listener=False):
     browser = webdriver.Chrome(chrome_options=get_options(data_abs_path))
 
     print("Going to www.askform.cn/login ...")
-    browser.set_page_load_timeout(30)
+    browser.set_page_load_timeout(600)
     browser.get("https://www.askform.cn/login")
 
     print("Loggin in...")
