b056885a1bc9693f7fcf659573af432aad4eeca9
moujunlin <59753597+moujunlin@users.noreply.github.com>
2020-12-09 12:12:47 +0800

    Update matchAndSend.py

--- matchAndSend.py
+++ matchAndSend.py
@@ -6,7 +6,7 @@ import environment
 import pymongo
 
 developer_list = {"Aaron":"aaronwang0407@gmail.com", "Danny":"dannyding123456@gmail.com", "Jaya":"jessicahu819@hotmail.com",
-    "Tina": "yuto.dong@mail.utoronto.ca"}
+    "Tina": "yuto.dong@mail.utoronto.ca"，"Junlin": "104431605@qq.com"}
 
 def matchAndSend():
     #Get "relative path"
